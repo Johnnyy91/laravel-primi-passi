cc2a92d773bf6e0c0bfc8f3d9fd551727f6d2584
Johnnyy91 <claudiosardilli@gmail.com>
2022-10-27 15:44:24 +0200

    exercise complete

---
+++ laravel-primi-passi/resources/views/helloworld.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Helloworld</title>
+</head>
+<body>
+    <a href="{{url('/list')}}">LINK 1</a>
+    <h1>HELLO WORLD</h1>
+
+    @php
+        $Number = [1,2,4,7,8,3,4,7];
+    @endphp
+
+    @foreach($Number as $Numbers )
+        <p>{{$Numbers}}</p>
+    @endforeach
+
+</body>
+</html>
